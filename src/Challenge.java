a915abc9fa02afd1a0bc0f038fcdae27b2ed9ef8
anquf5 <anquf5@163.com>
2022-04-23 00:36:06 +0100

    2022-04-22

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,48 +1,6 @@
 import java.util.*;
 
 public class Challenge {
-    public static int solution(int[] A, String[] D) {
-        // write your code in Java SE 8
-        int num = A.length;
-        int[][] account = new int[12][2];
-        // 0 - the payment number of this month
-        // 1 - the total payment of this month
-        Set<Integer> deducMon = new HashSet<>();
-        for (int i = 0; i < num; i++) {
-            int m = Integer.valueOf(D[i].split("-")[1]) - 1;
-            if(A[i] < 0) {
-                account[m][0]++;
-                account[m][1] += A[i];
-            }
-            if(account[m][0] >= 3 && account[m][1] <= -100) {
-                deducMon.add(m);
-            }
-        }
-        System.out.println(deducMon.size());
-        return Arrays.stream(A).sum() - (5 * (12 - deducMon.size()));
-    }
-    public static int findPeakElement(int[] nums) {
-        int l = 0, r = nums.length - 1;
-        List<Integer> list = new ArrayList<>();
-        findPeak(nums,l,r,list);
-        return (int)list.toArray()[0];
-    }
-
-    static void findPeak(int nums[], int li, int ri, List<Integer> list){
-        if(list.size()>0) return;
-        while(li < ri){
-            int mi = (li + ri) / 2;
-            if(mi == 0 || mi == nums.length-1) return;
-            if(nums[mi] > nums[mi-1] && nums[mi] > nums[mi+1]){
-                list.add(mi);
-                return;
-            }else{
-                findPeak(nums, li, mi, list);
-                findPeak(nums, mi, ri, list);
-            }
-        }
-        return;
-    }
 
     public static int minReorder(int n, int[][] connections) {
         HashSet<Integer>[] start = new HashSet[n];
@@ -82,96 +40,8 @@ public class Challenge {
         }
     }
 
-    public static int groupDivision(List<Integer> levels, int maxSpread) {
-        // Write your code here
-        int n = levels.size();
-        int[] student = new int[n];
-        for (int i = 0; i < n; i++) {
-            student[i] = levels.get(i);
-        }
-        Arrays.sort(student);
-        int lo = 0, hi = 1;
-        int res = 0;
-        while(hi < n){
-            if(student[hi] - student[lo] == maxSpread){
-                res++;
-                lo = hi + 1;
-                hi++;
-            }
-            if(student[hi] - student[lo] > maxSpread){
-                res++;
-                lo = hi;
-                hi++;
-            }
-            while(student[hi] - student[lo] < maxSpread){
-                hi++;
-            }
-        }
-        return res;
-    }
-    public static int numPaths(List<List<Integer>> warehouse) {
-        // Write your code here
-        int m = warehouse.size();
-        int n = warehouse.get(0).size();
-        int[][] dp = new int[m][n];
-        if(warehouse.get(0).get(0) == 0){
-            dp[0][0] = 0;
-        }
-        else dp[0][0] = 1;
-        for (int i = 1; i < m; i++) {
-            if(warehouse.get(i).get(0) == 1){
-                dp[i][0] = dp[i-1][0];
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            if(warehouse.get(0).get(i) == 1){
-                dp[0][i] = dp[0][i-1];
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if(warehouse.get(i).get(j) == 0) dp[i][j] = 0;
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-    }
-
-    public static int ArrayChallenge(int[] arr) {
-        // code goes here
-        int N = arr[0];
-        int len = arr.length - 1;
-        int[] median = new int[len];
-        int[] realArr = Arrays.copyOfRange(arr,1,len + 1);
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        median[0] = realArr[0];
-        list.add(realArr[0]);
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i < len; i++) {
-            list.add(realArr[i]);
-            if(list.size() > 3) list.remove(0);
-            median[i] = getMedian(new ArrayList<>(queue));
-        }
-        for (int i = 0; i < len; i++) {
-            System.out.println(median[i]);
-        }
-        return 0;
-    }
-    static int getMedian(List<Integer> subArr){
-        Collections.sort(subArr);
-        int subLen = subArr.size();
-        if(subLen % 2 == 0){
-            return (subArr.get(subLen / 2) + subArr.get(subLen / 2 - 1)) / 2;
-        }else{
-            return subArr.get(subLen / 2);
-        }
-    }
-
 
     public static void main(String[] args) {
-        int[] test = {3, 1, 3, 5, 10, 6, 4, 3, 1};
-        ArrayChallenge(test);
 
     }
 }
--- src/M202204/Solution.java
+++ src/M202204/Solution.java
@@ -610,29 +610,54 @@ class Solution {
         }
         return dp[n-1];
     }
-    //42. Trapping Rain Water
-    public int trap(int[] height) {
-        int[] dp = new int[height.length];
-        dp[0] = 0;
-        int l = 0;
-        int i = 0;
-        int button = 0;
-        while(i < height.length){
 
-        }
-        return 0;
+    //04-22
+    // 22. Generate Parentheses
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(res, new StringBuilder(), 0, 0, n);
+        return res;
     }
 
+    static void backtrack(List<String> res, StringBuilder string, int open, int close, int n){
+        if(string.length() == 2 * n){
+            res.add(string.toString());
+            return;
+        }
+        if(open < n){
+            string.append('(');
+            backtrack(res, string, open+1, close, n);
+            string.deleteCharAt(string.length()-1);
+        }
+        if(close < open){
+            string.append(')');
+            backtrack(res, string, open, close+1, n);
+            string.deleteCharAt(string.length()-1);
+        }
+    }
 
-
-
-
+    // 64. Minimum Path Sum
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 
     public static void main(String[] args) {
-        String s1 = "babad";
-        String s2 = "cbbd";
-        String s3 = "ac";
-        String s4 = "ccc";
-        System.out.println(longestPalindrome(s4));
+        int[][] test1 = {{1,3,1},{1,5,1},{4,2,1}};
+        int[][] test2 = {{1,2,3},{4,5,6}};
+        System.out.println(minPathSum(test1));
     }
 }
